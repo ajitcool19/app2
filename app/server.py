f27f3d63824b0665bee8c127489e9ca4fd3eaa7c
Ajit Bhalerao <ajitbhalerao74@gmail.com>
2019-09-28 22:13:12 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1_-cazrblgXbJaEe_82ShvW5ss6Zx10PP'
+export_file_name = '300-stage2.pth'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Abyssinian', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
